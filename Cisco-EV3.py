eb06f900ad8b3815a2773244d7281224f2b08f98
Michael <michmedina@hotmail.cl>
2022-08-19 00:11:03 -0400

    Segundo commit - Se obtiene IP de los dispositivos

--- Cisco-EV3.py
+++ Cisco-EV3.py
@@ -2,6 +2,7 @@ import requests
 import json
 import conf
 
+sandbox = "https://10.10.20.14"
 
 def obtener_token(usuario, clave):
     url = "https://10.10.20.14/api/aaaLogin.json"
@@ -21,6 +22,22 @@ def obtener_token(usuario, clave):
     token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
     return token
 
+# GET http://apic-ip-address/api/class/topSystem.json
+# obtener_token(conf.usuario, conf.clave)
+def top_system():
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    clavetoken = {
+        "APIC-Cookie": obtener_token(conf.usuario, conf.clave)
+    }
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.get(sandbox+"/api/class/topSystem.json", headers=cabecera, cookies=clavetoken, verify=False)
 
-token = obtener_token(conf.usuario, conf.clave)
-print(token)
+    total_nodos = int(respuesta.json()["totalCount"])
+
+    for i in range(0, total_nodos):
+        ip_local = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["address"]
+        print(ip_local)
+
+top_system()
