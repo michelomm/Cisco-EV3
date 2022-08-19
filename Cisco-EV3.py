da7af76a1f8967f38c1b08b307160179ffecb0ba
Michael <michmedina@hotmail.cl>
2022-08-19 01:36:14 -0400

    Tercer commit - Se obtiene Nombre, IP, MAC, Estado y Tiempo de actividad de los dispositivos - Se agrega try y except ante error de conectividad.

--- Cisco-EV3.py
+++ Cisco-EV3.py
@@ -2,10 +2,10 @@ import requests
 import json
 import conf
 
-sandbox = "https://10.10.20.14"
 
+# GET https://IP_Sambox/api/aaaLogin.json
 def obtener_token(usuario, clave):
-    url = "https://10.10.20.14/api/aaaLogin.json"
+    url = conf.sandbox + "/api/aaaLogin.json"
     body = {
         "aaaUser": {
             "attributes": {
@@ -18,12 +18,16 @@ def obtener_token(usuario, clave):
         "Content-Type": "application/json"
     }
     requests.packages.urllib3.disable_warnings()
-    respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
+    try:
+        respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
+    except Exception as err:
+        print("Error al consumir el API por problema de conectividad")
+        exit(1)
     token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
     return token
 
-# GET http://apic-ip-address/api/class/topSystem.json
-# obtener_token(conf.usuario, conf.clave)
+
+# GET https://IP_Sambox/apic-ip-address/api/class/topSystem.json
 def top_system():
     cabecera = {
         "Content-Type": "application/json"
@@ -32,12 +36,20 @@ def top_system():
         "APIC-Cookie": obtener_token(conf.usuario, conf.clave)
     }
     requests.packages.urllib3.disable_warnings()
-    respuesta = requests.get(sandbox+"/api/class/topSystem.json", headers=cabecera, cookies=clavetoken, verify=False)
-
+    respuesta = requests.get(conf.sandbox + "/api/class/topSystem.json", headers=cabecera, cookies=clavetoken, verify=False)
     total_nodos = int(respuesta.json()["totalCount"])
 
     for i in range(0, total_nodos):
         ip_local = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["address"]
-        print(ip_local)
+        name = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["name"]
+        mac = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["fabricMAC"]
+        estado = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["state"]
+        uptime = respuesta.json()["imdata"][i]["topSystem"]["attributes"]["systemUpTime"]
+
+        print("Nombre: " + name + "\t" + "\t" +
+              "|    IP Address: " + ip_local + "   " + "\t" +
+              "|    MAC Address: " + mac + "\t" + "\t" +
+              "|    Estado: " + estado + "\t" + "\t" +
+              "|    Tiempo de actividad: " + uptime)
 
 top_system()
